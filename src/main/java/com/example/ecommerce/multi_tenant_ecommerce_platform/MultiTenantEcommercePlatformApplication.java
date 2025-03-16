8df9f3fa089c11ac96804374ebc8390d86593ab9
VedaSathvika <23wh1a6634@bvrithyderabad.edu.in>
2025-03-16 22:04:39 +0530

    First commit

---
+++ src/main/java/com/example/ecommerce/multi_tenant_ecommerce_platform/MultiTenantEcommercePlatformApplication.java
@@ -0,0 +1,13 @@
+package com.example.ecommerce.multi_tenant_ecommerce_platform;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MultiTenantEcommercePlatformApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MultiTenantEcommercePlatformApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/ecommerce/multi_tenant_ecommerce_platform/MultiTenantEcommercePlatformApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ecommerce.multi_tenant_ecommerce_platform;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MultiTenantEcommercePlatformApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
