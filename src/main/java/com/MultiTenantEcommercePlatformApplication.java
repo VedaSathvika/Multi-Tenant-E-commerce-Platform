e8e1707cf685f29de9720981d5be92674a38ab53
VedaSathvika <23wh1a6634@bvrithyderabad.edu.in>
2025-03-17 22:12:52 +0530

    Second commit

--- src/main/java/com/MultiTenantEcommercePlatformApplication.java
+++ src/main/java/com/MultiTenantEcommercePlatformApplication.java
@@ -1,4 +1,4 @@
-package com.example.ecommerce.multi_tenant_ecommerce_platform;
+package com;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
