206d77dd8ba3e38fcead89c514a034434f9d5ef4
Asha0509 <165293713+Asha0509@users.noreply.github.com>
2025-03-20 21:42:11 +0530

    Third Commit

--- src/main/java/com/MultiTenantEcommercePlatformApplication.java
+++ src/main/java/com/MultiTenantEcommercePlatformApplication.java
@@ -1,9 +1,9 @@
 package com;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.SpringBootConfiguration;
 
-@SpringBootApplication
+@SpringBootConfiguration
 public class MultiTenantEcommercePlatformApplication {
 
 	public static void main(String[] args) {
